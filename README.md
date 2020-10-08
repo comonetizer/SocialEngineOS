67c51aa5983d6b69ac9e4830501e1e02c3e54941
stefian <v_ianta@yahoo.com>
2020-10-08 09:30:17 -0300

    + <h1>Solutions @ AI speed</h1>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Politics AI - 24/7 Collaboration & Monetization Engine
+# Big O AI - 24/7 Collaboration & Monetization Engine
 
 collaboration engine / social & enterprise innovation protocol - PoC
 
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">Politics AI</router-link>
+      <router-link to="/">Big O AI</router-link>
     </div>
     <nav>
       <ul>
