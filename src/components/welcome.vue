4d8df52e0bc46ec962414a32d2e392cfb0d0c001
stefian <v_ianta@yahoo.com>
2020-10-13 08:45:58 -0300

    +     <h1>Terra Team @ AI speed</h1>     <p>collaborate compete monetize 24/7 - 3rd gen. DNA OS PoC</p>

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="welcome">
-    <h1>Terra Teams @ AI speed</h1>
-    <p>collaborate compete monetize 24/7 - 3rd gen DNA OS</p>
+    <h1>Terra Team @ AI speed</h1>
+    <p>collaborate compete monetize 24/7 - 3rd gen. DNA OS PoC</p>
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
       <router-link to="/signin">Sign In</router-link>
