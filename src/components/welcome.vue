81d6217c341d18db18947767cdad2d629ced86ce
stefian <v_ianta@yahoo.com>
2020-10-16 07:58:54 -0300

    + <p>collaborate compete monetize 24/7 - 4th gen DNA OS</p>

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="welcome">
     <h1>Thinking @ AI speed</h1>
-    <p>collaborate compete monetize 24/7 - 4rd gen DNA OS</p>
+    <p>collaborate compete monetize 24/7 - 4th gen DNA OS</p>
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
       <router-link to="/signin">Sign In</router-link>
