d1bda47ea681b8b9bea73f3fe2c69e0ea47cf09c
stefian <v_ianta@yahoo.com>
2020-10-21 19:18:05 -0300

    + <title>Attn: AI PoC</title>

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">attn: AI</router-link>
+      <router-link to="/">Attn: AI</router-link>
     </div>
     <nav>
       <ul>
