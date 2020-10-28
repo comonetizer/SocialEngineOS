0d66d0413c994933011f27e2bd74a8de9b087110
stefian <v_ianta@yahoo.com>
2020-10-28 12:26:56 -0300

    + <h1>Business @ AI speed</h1>

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="welcome">
-    <h1>Monetize @ AI speed</h1>
+    <h1>Business @ AI speed</h1>
     <p>coordinate > monetize > repeat 24/7 - 3rd gen. DNA OS</p>
     <div class="cta">
       <router-link to="/signup">Sign Up</router-link>
