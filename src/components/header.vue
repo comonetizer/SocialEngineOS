725aebe8e700c9dbf8b7251c9a3793c0375460c7
stefian <v_ianta@yahoo.com>
2020-10-31 13:40:37 -0300

    + <h1>Terra Pros @ AI Speed</h1>

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="logo">
-      <router-link to="/">DevEngine OS</router-link>
+      <router-link to="/">Terra Pros OS</router-link>
     </div>
     <nav>
       <ul>
